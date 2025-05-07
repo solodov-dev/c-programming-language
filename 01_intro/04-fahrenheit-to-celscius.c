c9c4d25652abc794b10adf5e84156fd03d4262dc
Andrey Solodov <solodov.dev@gmail.com>
2025-05-07 09:12:26 +0300

    Chapter 1; ex 1-4

---
+++ 01_intro/04-fahrenheit-to-celscius.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+// Output celsius and fahrenheit temperatures in a table format
+
+int main() {
+  float fahr, celsius;
+  int lower, upper, step;
+
+  lower = 0;
+  upper = 300;
+  step = 20;
+
+  celsius = lower;
+
+  printf(" C     F \n");
+  printf("__________\n");
+  while (celsius <= upper) {
+    fahr = 9.0 / 5.0 * celsius + 32.0;
+    printf("%3.0f %6.1f\n", celsius, fahr);
+    celsius += step;
+  }
+}
