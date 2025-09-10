5de8d5d1a575519527a2052227b6efa6029b7964
Andrey Solodov <solodov.dev@gmail.com>
2025-09-10 09:49:36 +0300

    Chapter 4; ex 1

---
+++ 04-functions-and-program-structure/01-strrindex.c
@@ -0,0 +1,34 @@
+#include <assert.h>
+#include <stdio.h>
+#include <string.h>
+
+void test_strrindex();
+void reverse(char s[], int len);
+
+int main() { test_strrindex(); }
+
+int strrindex(char s[], char t[])
+{
+  int i, j, k;
+  int len_s = strlen(s) - 1;
+  int len_t = strlen(t) - 1;
+
+  for (i = len_s; i >= 0; i--) {
+    for (j = i, k = len_t; k >= 0 && s[j] == t[k]; k--, j--)
+      ;
+
+    if (k == -1 && i != -1)
+      return i - len_t;
+  }
+
+  return -1;
+}
+
+void test_strrindex()
+{
+  assert(strrindex("abcdef", "def") == 3);
+  assert(strrindex("abcdefalksjdf;akjf", "bcd") == 1);
+  assert(strrindex("abcdef", "abc") == 0);
+  assert(strrindex("abcdef", "ghl") == -1);
+  printf("All 'strrindex(int n, char s[])' tests has passed successfully\n");
+}
